3a6bd5e8a46a06cca3cc7023901ca0a90d9bbaa0
charmiainemimie <charmainemangorima@gmail.com>
2024-05-08 08:49:25 +0200

    added json middleware

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const workoutRoutes=require('./routes/workouts')
 
 const PORT = process.env.PORT || 4000; // Use the port from the environment variable or default to 400
 
+//START ADDING MIDDLEWARE
+//middleware for json-for all requests that send data to the server
+app.use(express.json())
+
 // middleware to get the path and method for the request
 app.use((req,res,next)=>{
     console.log('path = '+req.path, 'and method = '+req.method)
@@ -17,6 +21,8 @@ app.use((req,res,next)=>{
   
 //middleware to use the routes
 app.use('/api/workouts', workoutRoutes)
+//END OF MIDDLEWARE
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
