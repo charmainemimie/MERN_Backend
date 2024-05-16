fc7125ad83e617028c55e973a974d2e013b9c125
charmiainemimie <charmainemangorima@gmail.com>
2024-05-16 11:53:15 +0200

    setup user controller

---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express=require('express')
+
+
+//controller functions
+const {loginUser,signupUser}=require('../controllers/userController')
+
+const router=express.Router()
+
+//login router
+router.post('/login',loginUser)
+
+//signup router
+router.post('/signup ',signupUser)
