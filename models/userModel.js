25a331df9d5e7f1569c5ec0657a591f727019ad0
charmiainemimie <charmainemangorima@gmail.com>
2024-05-17 13:55:57 +0200

    static signup method

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
-
 const Schema = mongoose.Schema
+const bcrypt=require('bcrypt')
 
 //SCHEMA -takes 2 args--- define a document structure and set timestanps
 const userSchema=new Schema({
@@ -15,5 +15,17 @@ const userSchema=new Schema({
     },
   
 },{timestamps:true})
+
+//static signup method
+userSchema.statics.signup=async (email,password)=>{
+    const exists=await this.findOne({email})
+
+    if(exists){
+        throw Error('Email already in use')
+    }
+
+    //hashing password using bcrypt
+    
+}
 
 module.exports=mongoose.model('User',userSchema)
